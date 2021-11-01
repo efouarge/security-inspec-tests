d5d7c3502c436d653fed3636c7415245c03b1daf
Eric Fouarge <eric.fouarge@gmail.com>
2021-11-01 14:17:56 -0500

    fixing inspec test

--- rapid7/main.rb
+++ rapid7/main.rb
@@ -1,6 +1,16 @@
 ### This Inspec test confirms that Rapid7 Service is installed and running
 
-describe service('ir_agent') do
-  it { should be_installed }
-  it { should be_running }
+control 'Rapid7 Agent is Installed and running' do
+  title 'R7 Agent Installed'
+  desc 'Rapid 7 Agent is Installed and Running'
+  impact 0.9
+  require 'rbconfig'
+  is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+  if is_windows
+    describe service('ir_agent') do
+      it { should be_installed }
+      it { should be_running }
+    end
+  end
 end
+i
